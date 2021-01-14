348db63147e46ce1f9b6c8909fe09b030d4ea159
Gyujin Lee <ryanlee5646@gmail.com>
2021-01-15 00:53:48 +0900

    20210115 web-scraper get company

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from indeed import extract_indeed_pages, extract_indeed_jobs
 # 마지막 페이지를 가져오는 Function
 last_indeed_pages = extract_indeed_pages()
 
+
 # 모든 일자리를 반환하는 함수
 indeed_jobs = extract_indeed_jobs(last_indeed_pages)
 
 
 #### 모든 페이지에 job title 추출
+

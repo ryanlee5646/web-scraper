d93396756e3c6bd4073fed8a3082ae98a1510711
Gyujin Lee <ryanlee5646@gmail.com>
2021-02-04 21:47:14 +0900

    20200204 export csv

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from scrapper import get_jobs
 
 app = Flask("SuperScrapper")
 
+# Fake Database
+db = {}
+
 @app.route("/")
 def home():
     return render_template("potato.html")
@@ -12,11 +15,24 @@ def report():
     word = request.args.get('word')
     if word:
         word = word.lower()
-        # jobs = get_jobs(word)
-        # print(jobs)
+        exixtingJobs = db.get(word)
+        # db에 검색한 job이 존재하면 기존의 job
+        # if fromDb:
+            # jobs = fromDb
+        if  exixtingJobs:
+            jobs = exixtingJobs
+        # 없으면 Scrap    
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+        
     else: 
         return redirect("/")
-    return render_template("report.html", word=word, potato="sexy")
+    return render_template("report.html", 
+    word=word, 
+    resultsNumber=len(jobs),
+    jobs=jobs
+    )
 
 
 app.run(host="0.0.0.0")
